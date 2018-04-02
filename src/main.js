93b7bf14636ebeca8d23d1364a4a8f6c1c6b8c17
bluesbonew <bluesbonewong@gmail.com>
2018-04-02 22:06:30 +0800

    引入 element ui
    引入 element ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import App from './App'
 import 'normalize.css'
 import './assets/reset.scss'
 
+// 引入 element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
