874c60aa0cde2df7d9ed078ebfd6a68eacf6f98d
bluesbonew <bluesbonewong@gmail.com>
2018-04-02 21:46:44 +0800

    把页面氛围三块二

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Topbar class="topbar"/>
+    <main>
+      <Editor class="editor"/>
+      <Preview class="preview"/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import HelloWorld from './components/HelloWorld'
+  import Topbar from './components/Topbar'
+  import Editor from './components/Editor'
+  import Preview from './components/Preview'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {
+      HelloWorld,
+      Topbar,
+      Editor,
+      Preview
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+  }
+
+  main {
+    display: flex;
+    flex: 1;
+
+    > .editor {
+      width: 30em;
+    }
+    > .preview {
+      flex: 1;
+    }
+  }
+
+
 </style>
